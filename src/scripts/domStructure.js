7dc30ced018c2d587de43597dc679924d8c298cc
SamBritt <sam.britt2@yahoo.com>
2019-04-03 15:59:19 -0500

    created function in domStructure that builds form labels and input elements. Created save Button that logs a message to the console.

--- src/scripts/domStructure.js
+++ src/scripts/domStructure.js
@@ -1,10 +1,28 @@
 import build from "./constructors"
 
 const domStructure = {
-  buildTaskHTML (task) {
-    buildElement("div", `task--${task.id}`);
-    
+  buildTaskFormButton() {
+    let createTaskButton = build.buttonCreator("taskButtonTask", "Create New Task", undefined);
+    createTaskButton.addEventListener("click", () => console.log("Clicked!"));
+  },
+  buildTaskForm() {
+    const form = build.elementWithTextCreator("form", undefined, "buildFormTask", undefined);
 
+    const labelForName = build.elementWithTextCreator("label", "Enter Task Name: ", undefined, undefined);
+    let inputName = build.inputCreator("text", "nameInputTask");
+    form.appendChild(labelForName);
+    form.appendChild(inputName);
+
+    const labelForDate = build.elementWithTextCreator("label", "Enter Date: ", undefined, undefined);
+    let inputDate = build.inputCreator("date", "dateInputTask");
+    form.appendChild(labelForDate)
+    form.appendChild(inputDate)
+
+    let formSubmitButton = build.buttonCreator("submitFormTask", "Submit Task", undefined);
+    formSubmitButton.addEventListener("click", () => console.log("Clicked that ish"));
+    form.appendChild(formSubmitButton);
+
+    return form;
   }
 }
 
