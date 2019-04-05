c78166e2a0d1ce744529d9290e764c33f99a23cc
Rose Wisotzky <rose@rosewisotzky.com>
2019-04-05 14:09:32 -0500

    Targeted input values and console logged them

--- src/scripts/eventsTabHandlers.js
+++ src/scripts/eventsTabHandlers.js
@@ -2,24 +2,18 @@ import eventsHTML from "./eventsDomManager"
 
 const eventsHandler = {
     // Event handler to print new event form to the DOM
-    addNewEventHandler () {
+    addNewEventHandler() {
         // Selecting the element with the class is--hidden and storing it in the variable formSection
         let formSection = document.querySelector(".is--hidden")
         // Removes the class is--hidden from the element formSection
         formSection.classList.remove("is--hidden")
     },
-    saveEventHandler () {
-        let eventDisplay = document.querySelector("#events-container")
+    saveEventHandler() {
         let eventName = document.querySelector("#newEvent").value;
         let eventDate = document.querySelector("#newEventDate").value;
         let eventLocation = document.querySelector("#eventLocation").value;
-        eventDisplay.appendChild(eventName)
-        eventDisplay.appendChild(eventDate)
-        eventDisplay.appendChild(eventLocation)
-        return eventDisplay
-
-
+        console.log(eventName, eventDate, eventLocation)
     }
 }
 
-export default eventsHandler 
+export default eventsHandler
