6a0db4f7df9068b7007ea861b0f2d4627816989d
Rose Wisotzky <rose@rosewisotzky.com>
2019-04-08 09:58:48 -0500

    Working on GET call to post events to the dom

--- src/scripts/listEvents.js
+++ src/scripts/listEvents.js
@@ -1,7 +1,16 @@
 import API from "./eventsAPIManager"
+import build from "./domStructure"
 
 export default {
     listAllEvents () {
         API.getAllEvents().then(eventResponse => console.log("event response", eventResponse))
+        const eventsFragment = document.createDocumentFragment();
+
+        eventsResponse.forEach((event, indexNumber) => {
+            const eventHTML = event.build.
+
+        })
+
+
     }
 }
